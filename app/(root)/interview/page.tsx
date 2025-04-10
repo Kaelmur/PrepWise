fc351add5738fbef29965bc0058fbe92330e7588
Kael <a.murataliev@list.ru>
2025-04-10 13:46:44 +0500

    interview generation page iu

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+
+function Page() {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="Kael" userId="user1" type="generate" />
+    </>
+  );
+}
+
+export default Page;
